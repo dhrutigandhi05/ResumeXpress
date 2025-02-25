54d0bfc4728678fffc60119ff5159269679cc2eb
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-02-24 19:06:08 -0500

    updated to python

---
+++ backend/venv/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+# from routes import auth, resume
+
+app = FastAPI()
+
+# app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
+# app.include_router(resume.router, prefix="/api/resume", tags=["resume"])
+
+@app.get("/")
+def home():
+    return {"message": "ResumeXpress FastAPI Backend Running!"}
