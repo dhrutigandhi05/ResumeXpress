b63daf46954ca50e0e47925dbc09898313eee006
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-02-24 20:32:17 -0500

    added code for secret hash

--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,29 @@
 import os
 from dotenv import load_dotenv
 import boto3
+import base64
+import hmac
+import hashlib
 
 load_dotenv()
 
 COGNITO_REGION = os.getenv("COGNITO_REGION")
 COGNITO_USER_POOL_ID = os.getenv("COGNITO_USER_POOL_ID")
 COGNITO_CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
+COGNITO_CLIENT_SECRET = os.getenv("COGNITO_CLIENT_SECRET")
 
-print("COGNITO_USER_POOL_ID:", COGNITO_USER_POOL_ID)
-print("COGNITO_CLIENT_ID:", COGNITO_CLIENT_ID)
-print("COGNITO_REGION:", COGNITO_REGION)
+# print("COGNITO_USER_POOL_ID:", COGNITO_USER_POOL_ID)
+# print("COGNITO_CLIENT_ID:", COGNITO_CLIENT_ID)
+# print("COGNITO_REGION:", COGNITO_REGION)
 
 cognito_client = boto3.client("cognito-idp", region_name=COGNITO_REGION)
+
+def calculate_secret_hash(username):
+    message = username + COGNITO_CLIENT_ID
+    dig = hmac.new(
+        COGNITO_CLIENT_SECRET.encode("utf-8"),
+        message.encode("utf-8"),
+        hashlib.sha256
+    ).digest()
+
+    return base64.b64encode(dig).decode()
--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -7,6 +7,7 @@ router = APIRouter()
 class UserSignup(BaseModel):
     email: str
     password: str
+    name: str
 
 class UserLogin(BaseModel):
     email: str
@@ -15,11 +16,16 @@ class UserLogin(BaseModel):
 @router.post("/signup")
 def signup(user: UserSignup):
     try:
+        secret_hash = config.calculate_secret_hash(user.email)
         reponse = config.cognito_client.sign_up(
             ClientId=config.COGNITO_CLIENT_ID,
+            SecretHash=secret_hash,
             Username=user.email,
             Password=user.password,
-            UserAttributes=[{"Name": "email", "Value": user.email}],
+            UserAttributes=[
+                {"Name": "email", "Value": user.email},
+                {"Name": "name", "Value": user.name},
+            ],
         )
 
         return {"message": "Signup was successful"}
@@ -29,10 +35,11 @@ def signup(user: UserSignup):
 @router.post("/login")
 def login(user: UserLogin):
     try:
+        secret_hash = config.calculate_secret_hash(user.email)
         response = config.cognito_client.initiate_auth(
             ClientId=config.COGNITO_CLIENT_ID,
             AuthFlow="USER_PASSWORD_AUTH",
-            AuthParameters={"USERNAME": user.email, "PASSWORD": user.password},
+            AuthParameters={"USERNAME": user.email, "PASSWORD": user.password, "SECRET_HASH": secret_hash},
         )
 
         return {"token": response["AuthenticationResult"]["IdToken"]}
