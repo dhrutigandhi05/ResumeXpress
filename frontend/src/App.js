92ec8a26eef301986e8db49a8becd9da8568639d
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-01 20:48:17 -0500

    aws amplify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import {Amplify} from "aws-amplify";
+import awsConfig from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
-function App() {
+Amplify.configure(awsConfig);
+
+const App = ({signOut, user}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <button onClick={signOut}>Sign Out</button>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage user={user}/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
