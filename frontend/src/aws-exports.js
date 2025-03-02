0d283c71c465af770d382905b2893519d1bddfbc
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-01 19:24:16 -0500

    configuring aws cognito in react

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+    Auth: {
+        region: "ca-central-1",
+        userPoolId: "ca-central-1_6xyTF7aYM",
+        userPoolWebClientId: "5u19hb6l1ls3lmphce1l1me4bn",
+        authenticationFlowType: "USER_PASSWORD_AUTH",
+    },
+};
