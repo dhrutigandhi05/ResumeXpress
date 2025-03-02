9821b0a0325b389f7d89ad09dc324a95d4c8694e
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-01 20:54:10 -0500

    test

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -2,7 +2,7 @@ const awsConfig = {
     Auth: {
         region: process.env.REACT_APP_AWS_REGION,
         userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-        userPoolWebClientId: REACT_APP_COGNITO_CLIENT_ID,
+        userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
         authenticationFlowType: "USER_PASSWORD_AUTH",
     },
 };
