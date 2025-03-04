4916f766238a684320e07134fec8c4eeaa6ef2e6
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-03 20:45:56 -0500

    signup functionality

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -9,6 +9,19 @@ const Signup = () => {
     const [message, setMessage] = useState("");
 
     const handleSubmit = (event) => {
-        
-    }
+        event.preventDefault();
+
+        const attributeList = [
+            new CognitoUserAttribute({Name: "email", Value: email}),
+            new CognitoUserAttribute({Name: "name", Value: name}),
+        ];
+
+        UserPool.Signup(email, password, attributeList, null, (err, data) =>{
+            if (err) {
+                setMessage(`Error: ${err.message}`);
+            } else {
+                setMessage("Signup was successful. Check your email for the confirmation code");
+            }
+        });
+    };
 };
