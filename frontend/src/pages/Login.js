626f79eac1d5d4f3ec1d5af013014f2ab59a02a9
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-03 21:23:02 -0500

    login functionality

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,4 +9,42 @@ const Login = ({setUser}) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
-}
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const user = new CognitoUser({
+            Username: email,
+            Pool: UserPool,
+        });
+
+        const authDetails = new AuthenticationDetails({
+            Username: email,
+            Password: password,
+        });
+
+        user.authenticateUser(authDetails, {
+            onSuccess: (data) => {
+                setUser(data);
+                setMessage("Login Successful");
+            },
+            
+            onFailure: (err) => {
+                setMessage(`Error: ${err.message}`);
+            },
+        });
+    };
+
+    return (
+        <LoginForm
+            handleSubmit={handleSubmit}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            message={message}
+        />
+    );
+};
+
+export default Login;
