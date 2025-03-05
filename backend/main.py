10317e8893402ad8d4903f3a0859a955d99e3acf
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-04 21:24:24 -0500

    debugging

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from routes import auth
-# , resume
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
-# app.include_router(resume.router, prefix="/api/resume", tags=["resume"])
 
 @app.get("/")
 def home():
