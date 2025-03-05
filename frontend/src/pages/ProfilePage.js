3497ab760b51dcd419a0bed1ac946c99dd90ca6b
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-04 20:59:02 -0500

    calls to use fatsapi

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API_URL from "../config";
+import axios from "axios";
 import ProfileView from "../components/ProfileView";
-import UserPool from "../cognito";
 
-const ProfilePage = ({user, setUser}) => {
-    const handleSignOut = () => {
-        const currentUser = UserPool.getCurrentUser();
+const ProfilePage = ({ user }) => {
+    const [userData, setUserData] = useState(null);
+    const [message, setMessage] = useState("");
 
-        if (currentUser) {
-            currentUser.signOut();
-            setUser(null);
+    useEffect(() => {
+        if (user) {
+            axios.get(`${API_URL}/protected`, {
+                headers: { Authorization: `Bearer ${user.token}` },
+            })
+            .then((response) => setUserData(response.data.user))
+            .catch((error) => setMessage("Failed to load user data."));
         }
-    };
+    }, [user]);
 
-    return <ProfileView user={user} handleSignOut={handleSignOut} />;
+    return (
+        <ProfileView user={userData} />
+    );
 };
 
 export default ProfilePage;
