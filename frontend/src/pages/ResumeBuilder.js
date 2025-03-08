a62e32ae863aaf54f3cf3622f40ee18d5229e8ff
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-07 21:56:20 -0500

    functionality

--- frontend/src/pages/ResumeBuilder.js
+++ frontend/src/pages/ResumeBuilder.js
@@ -10,4 +10,23 @@ const ResumeBuilder = () => {
     const [experience, setExperience] = useState([]);
     const [skills, setSkills] = useState([]);
     const [education, setEducation] = useState([]);
-}
+
+    const handleAddExperience = () => {
+        setExperience([...experience, {company: '', position: '', description: ''}]);
+    };
+
+    const handleGenerateDescription = async (index) => {
+        const response = await fetch('http://127.0.0.1:8000/generate-description', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({input: experience[index].position})
+        });
+
+        const data = await response.json();
+        const updatedExperience = [...experience];
+        updatedExperience[index].description = data.generated_description;
+        setExperience(updatedExperience);
+    };
+
+    
+};
